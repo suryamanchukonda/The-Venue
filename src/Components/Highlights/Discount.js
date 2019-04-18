36d3e4ef64beeaff90fc0367ebe2358e09770d39
suryamanchukonda <manchusurya@gmail.com>
2019-04-18 18:10:04 +0530

    Added a reusable button component

--- src/Components/Highlights/Discount.js
+++ src/Components/Highlights/Discount.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide'; 
+
+import MyButton from '../Utils/MyButton';
 
 class discount extends Component {
 
@@ -40,9 +43,12 @@ class discount extends Component {
                             <h3>Purcahse tickets before 20th june</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         
-                            <div>
-                                button
-                            </div>
+                            <MyButton
+                                text="Purchase tickets"
+                                bck="#ffa800"
+                                color="#ffffff"
+                                link="http://google.com"
+                            />
 
                         </div>
                     </Slide>
---
+++ src/Components/Utils/MyButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+import TicketIcon from '../../resources/images/icons/ticket.png';
+
+const MyButton = (props) => {
+    return (
+        <Button
+            href= {props.link}
+            variant="contained"
+            size="small"
+            style={{
+                background: props.bck,
+                color: props.color
+            }}
+        >
+            <img src={TicketIcon}
+                 alt="icon_button"
+                 className="iconImage"
+            />
+            {props.text}
+        </Button>
+    );
+};
+
+export default MyButton;
